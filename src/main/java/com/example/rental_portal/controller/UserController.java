9a9e627f77338ce7d0f2e5a3e0641425dd9aab42
Thomas <thomas.mirabile@ynov.com>
2024-07-24 09:14:28 +0200

    first commit

---
+++ src/main/java/com/example/rental_portal/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.rental_portal.controller;
+
+import com.example.rental_portal.entity.User;
+import com.example.rental_portal.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/auth")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
---
+++ src/test/java/com/example/rental_portal/RentalPortalApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.example.rental_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
